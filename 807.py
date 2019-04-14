102ae7d4cc30358f9f22053664f895142d730146
xih108 <xih108@ucsd.edu>
2019-04-14 00:08:51 -0700

    Update 807.py

--- 807.py
+++ 807.py
@@ -1,3 +1,10 @@
+#807. Max Increase to Keep City Skyline
+#At the end, the "skyline" when viewed from all four directions of the grid, i.e. top, bottom, left, and right, 
+#must be the same as the skyline of the original grid. 
+#What is the maximum total sum that the height of the buildings can be increased?
+
+#First get the skyline but get the max, then foe row and col get min, then sum the heights added.
+
 class Solution:
     def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int:
         left = [max(i) for i in grid]
