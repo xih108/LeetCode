4ae84be4342a922f607c332794c2bfb728f9b125
xih108 <xih108@ucsd.edu>
2019-04-15 00:02:17 -0700

    Update 804.py

--- 804.py
+++ 804.py
@@ -1,3 +1,8 @@
+#804.Unique Morse Code Words
+#Return the number of different transformations among all words we have.
+
+#Use a set to get the number of different transformations.
+
 class Solution:
     def uniqueMorseRepresentations(self, words: List[str]) -> int:
         morse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
