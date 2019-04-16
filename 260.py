407a1dcc9807e3b4068250bc48a996daeeb07d40
xih108 <xih108@ucsd.edu>
2019-04-15 23:34:21 -0700

    Update 260.py

--- 260.py
+++ 260.py
@@ -3,30 +3,3 @@
 
 #Every time put a number into the set if its first appearence, otherwise remove it from the set. At last, return the list.
 
-    /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
-class Solution {
-public:
-    TreeNode* invertTree(TreeNode* root) {   
-        recursion(root);
-        return root;
-    }
-    
-    void recursion(TreeNode* root){
-        if (!root){
-            return;
-        }
-        TreeNode* new_left = root->right;
-        root->right = root->left;
-        root->left = new_left;
-        recursion(root->left);
-        recursion(root->right);
-    }
-};
