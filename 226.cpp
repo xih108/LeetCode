0b4177ec358cc7c189dc742c98f617795fa2fdfb
xih108 <xih108@ucsd.edu>
2019-04-15 23:31:48 -0700

    Update 226.cpp

--- 226.cpp
+++ 226.cpp
@@ -1,4 +1,9 @@
-/**
+#260. Single Number III
+#Given an array of numbers nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once.
+
+#Every time put a number into the set if its first appearence, otherwise remove it from the set. At last, return the list.
+
+    /**
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
