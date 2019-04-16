067efced72d9f159f3a82ffc36f98b3019a5ef7e
xih108 <xih108@ucsd.edu>
2019-04-15 23:33:38 -0700

    Update 260.py

--- 260.py
+++ 260.py
@@ -1,9 +1,32 @@
-class Solution:
-    def singleNumber(self, nums: List[int]) -> List[int]:
-        set1 = set()
-        for i in nums:
-            if i in set1:
-                set1.remove(i)
-            else:
-                set1.add(i)
-        return list(set1)
+#260. Single Number III
+#Given an array of numbers nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once.
+
+#Every time put a number into the set if its first appearence, otherwise remove it from the set. At last, return the list.
+
+    /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {   
+        recursion(root);
+        return root;
+    }
+    
+    void recursion(TreeNode* root){
+        if (!root){
+            return;
+        }
+        TreeNode* new_left = root->right;
+        root->right = root->left;
+        root->left = new_left;
+        recursion(root->left);
+        recursion(root->right);
+    }
+};
