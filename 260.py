2f157ac249c34649ab5585af8833932e0b85e8e5
xih108 <xih108@ucsd.edu>
2019-04-15 23:37:12 -0700

    Update 260.py

--- 260.py
+++ 260.py
@@ -3,3 +3,12 @@
 
 #Every time put a number into the set if its first appearence, otherwise remove it from the set. At last, return the list.
 
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        set1 = set()
+        for i in nums:
+            if i in set1:
+                set1.remove(i)
+            else:
+                set1.add(i)
+        return list(set1)
