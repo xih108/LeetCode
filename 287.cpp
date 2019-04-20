ddea7d00b96aa32dfbb86b02028b0ed6ac129293
xih108 <xih108@ucsd.edu>
2019-04-19 23:05:01 -0700

    Update 287.cpp

--- 287.cpp
+++ 287.cpp
@@ -1,3 +1,8 @@
+// 287. Find the Duplicate Number
+// Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
+
+// Use a set, if the element is already in the set, then return that number.
+
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
