26babbddabc8a4d46b69fe8ec1f09df821234fc4
xih108 <xih108@ucsd.edu>
2019-04-20 23:09:37 -0700

    Update 102.cpp

--- 102.cpp
+++ 102.cpp
@@ -1,3 +1,13 @@
+// 102. Binary Tree Level Order Traversal
+// Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+// For example:
+// Given binary tree [3,9,20,null,null,15,7],
+
+// Use a queue for BST, level by level,every time pop the curr,push to a vector, add its left and right,  ... the push this vector to a total vector.
+
+
+
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
