df85b57a7f94a72acb2725818d62e7ad231bcb52
xih108 <xih108@ucsd.edu>
2019-04-20 23:10:47 -0700

    Update 145.cpp

--- 145.cpp
+++ 145.cpp
@@ -1,3 +1,21 @@
+// 145. Binary Tree Postorder Traversal
+// Given a binary tree, return the postorder traversal of its nodes' values.
+
+// Example:
+
+// Input: [1,null,2,3]
+//    1
+//     \
+//      2
+//     /
+//    3
+
+// Output: [3,2,1]
+
+// Use recursion, left, right, root (DFS)
+
+
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
