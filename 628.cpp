d4354f1eaa2f6116f59e0d3fba4b5ea669177253
xih108 <xih108@ucsd.edu>
2019-04-26 22:45:50 -0700

    Update 628.cpp

--- 628.cpp
+++ 628.cpp
@@ -1,3 +1,8 @@
+// 628. Maximum Product of Three Numbers
+// Given an integer array, find three numbers whose product is maximum and output the maximum product.
+
+// Sort the numbers, the maximum would be max1*max2*max3 or min1*min2*max1    
+    
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
