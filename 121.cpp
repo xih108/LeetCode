4669a429143c968e205bb93215d576c055071139
xih108 <xih108@ucsd.edu>
2019-04-26 22:58:30 -0700

    Update 121.cpp

--- 121.cpp
+++ 121.cpp
@@ -1,3 +1,9 @@
+// 121. Best Time to Buy and Sell Stock
+// Say you have an array for which the ith element is the price of a given stock on day i.
+// If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+// everytime update the min stock price and the maxprofit currently.
+    
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
