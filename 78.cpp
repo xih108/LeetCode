2a05f736ef5ba97477d5d57ce2bd755ff13d3777
xih108 <xih108@ucsd.edu>
2019-04-28 22:35:49 -0700

    Update 78.cpp

--- 78.cpp
+++ 78.cpp
@@ -1,3 +1,16 @@
+// 78. Subsets
+// Given a set of distinct integers, nums, return all possible subsets (the power set).
+
+// First add [], then for each num in nums, keep the exisiting and add it to all the existing subsets.
+//example: [1,2,3]
+// [] 
+// add 1 [],[1]
+// add 2 [],[1],[2],[1,2]
+// add 3 [],[1],[2],[3],[1,3],[2,3],[1,2,3]
+    
+
+
+
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
