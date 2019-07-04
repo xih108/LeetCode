e44b80ce0a0c908ac0a55a8552f88225e0177871
xih108 <xih108@ucsd.edu>
2019-07-04 21:58:12 +0800

    Update 215.cpp

--- 215.cpp
+++ 215.cpp
@@ -1,3 +1,9 @@
+// 215. Kth Largest Element in an Array
+// Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+
+//Use priority queue as max heap, then pop until the k the largest element.
+
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
