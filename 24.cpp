39713ce8ca366d355ed28171ea953210c7a80860
xih108 <xih108@ucsd.edu>
2019-07-05 23:05:29 +0800

    Update 24.cpp

--- 24.cpp
+++ 24.cpp
@@ -1,3 +1,21 @@
+// 24. Swap Nodes in Pairs
+// Given a linked list, swap every two adjacent nodes and return its head.
+
+// You may not modify the values in the list's nodes, only nodes itself may be changed.
+
+ 
+
+// Example:
+
+// Given 1->2->3->4, you should return the list as 2->1->4->3.
+
+// Swap two adjacent nodes, and set the prev next to the its next, then change prev to be the next one.
+// (pretending each new prev as the head, no incoming link.)
+
+
+
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
