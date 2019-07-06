46a5e3f32cb2be9c3497dbc865461e2aadabe20e
xih108 <xih108@ucsd.edu>
2019-07-06 15:12:00 +0800

    Update 205.cpp

--- 205.cpp
+++ 205.cpp
@@ -1,3 +1,25 @@
+// 205. Isomorphic Strings
+// Given two strings s and t, determine if they are isomorphic.
+// Two strings are isomorphic if the characters in s can be replaced to get t.
+// All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character but a character may map to itself.
+// Example 1:
+
+// Input: s = "egg", t = "add"
+// Output: true
+// Example 2:
+
+// Input: s = "foo", t = "bar"
+// Output: false
+// Example 3:
+
+// Input: s = "paper", t = "title"
+// Output: true
+
+
+//Use an unorderedmap as a hashmap, each char corresponds to a unique char (no duplicates for the value implemented by using set).
+//If the length of the two strings not equal or the mapping doesn't match, return false, else return true.
+
+
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
