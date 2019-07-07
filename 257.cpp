2af26f98b2006221a5a5f1b69a2dd0da624675a1
xih108 <xih108@ucsd.edu>
2019-07-07 16:25:03 +0800

    Update 257.cpp

--- 257.cpp
+++ 257.cpp
@@ -1,3 +1,30 @@
+// 257. Binary Tree Paths
+// Given a binary tree, return all root-to-leaf paths.
+
+// Note: A leaf is a node with no children.
+
+// Example:
+
+// Input:
+
+//    1
+//  /   \
+// 2     3
+//  \
+//   5
+
+// Output: ["1->2->5", "1->3"]
+
+// Explanation: All root-to-leaf paths are: 1->2->5, 1->3
+
+// Using BFS implemented by queue, each element is a pair of the TreeNode and the string, everytime pop one and push its left and right 
+// node if there exists and update the string at the same time.
+// If it is a leaf node, then push the whole string to the vector.
+
+
+
+
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
